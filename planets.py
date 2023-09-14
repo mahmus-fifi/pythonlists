d3d97c9994e8126ccf878cb7d41be9bcff73f7b1
mahmus-fifi <mahmudshuaib555@gmail.com>
2023-09-14 08:56:06 +0100

    using lists and pprint

---
+++ planets.py
@@ -0,0 +1,6 @@
+"""working with python lists"""
+from pprint import pprint
+# lets create a list to hold all the planets, let's add pluto 
+planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupyter', 'Saturn', 'Uranus', 'Neptune'
+           ,'Pluto']
+pprint(f'The nine plantes are: {planets}')
